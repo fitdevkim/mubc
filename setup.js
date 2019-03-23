cbe50246e6bc76470f85961837b901136724c878
Muhammad Nurhakim Bin Margono <hakimzdope@gmail,com>
2019-03-23 21:25:38 +0800

    Added admin user setup

--- setup.js
+++ setup.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 const config = require("./config/database");
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
+
 const data = [
   {
     sectionType: "about-banksia",
@@ -28,6 +31,11 @@ const data = [
   }
 ];
 
+const adminUser = {
+  username: "admin",
+  password: "admin001"
+};
+
 // Connect to db
 mongoose.connect(config.database, { useNewUrlParser: true });
 let db = mongoose.connection;
@@ -43,34 +51,78 @@ db.once("open", () => {
 // Bring in about model
 let About = require("./models/aboutsection");
 
-// Check if about sections has been created
-About.find({}, (err, sections) => {
+// Bring in users model
+let User = require("./models/user");
+
+// Check if user has been created
+User.find({}, (err, user) => {
   if (err) {
     console.log(err);
     process.exit();
   } else {
-    if (sections.length === 0) {
-      let cnt = 0;
-      data.forEach(sect => {
-        let as = new About();
-        as.sectionType = sect.sectionType;
-        as.name = sect.name;
-        as.desc = sect.desc;
-        as.save(err => {
-          if (err) {
-            console.log(err);
-          } else {
-            console.log(`Section Type (${as.name}): Successfully saved to database`);
-            cnt++;
-            if (cnt == 5) {
-              console.log("Setup Done");
+    if (user.length === 0) {
+      let admin = new User(adminUser);
+
+      bcrypt.genSalt(10, (err, salt) => {
+        if (err) {
+          console.log(err);
+          process.exit();
+        } else {
+          bcrypt.hash(admin.password, salt, (err, hash) => {
+            if (err) {
+              console.log(err);
               process.exit();
             }
-          }
-        });
+            admin.password = hash;
+            admin.save(err => {
+              if (err) {
+                console.log(err);
+                process.exit();
+              } else {
+                console.log("Admin user successfully created.");
+                // Check if about sections has been created
+                About.find({}, (err, sections) => {
+                  if (err) {
+                    console.log(err);
+                    process.exit();
+                  } else {
+                    if (sections.length === 0) {
+                      let cnt = 0;
+                      data.forEach(sect => {
+                        let as = new About();
+                        as.sectionType = sect.sectionType;
+                        as.name = sect.name;
+                        as.desc = sect.desc;
+                        as.save(err => {
+                          if (err) {
+                            console.log(err);
+                          } else {
+                            console.log(
+                              `Section Type (${
+                                as.name
+                              }): Successfully saved to database`
+                            );
+                            cnt++;
+                            if (cnt == 5) {
+                              console.log("Setup Done");
+                              process.exit();
+                            }
+                          }
+                        });
+                      });
+                    } else {
+                      console.log("About Sections already exists in the database.");
+                      process.exit();
+                    }
+                  }
+                });
+              }
+            });
+          });
+        }
       });
     } else {
-      console.log("About Sections already exists in the database.");
+      console.log("Admin user already exists in the database.");
       process.exit();
     }
   }
