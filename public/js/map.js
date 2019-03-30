91ddea742cf56bc36be1ba175678981cb4144427
Muhammad Nurhakim Bin Margono <hakimzdope@gmail,com>
2019-03-30 20:47:22 +0800

    locate user exception bug fix

--- public/js/map.js
+++ public/js/map.js
@@ -93,7 +93,7 @@ class Map {
             // Check if user is within boundary
             if (!_isBounds(point, bounds)) {
               alert("You are not within the Banksia Court.");
-              map.setView(map._initialCenter, map._initialZoom);
+              map.setView(m.center, m.zoom);
             } else {
               const marker = L.marker([e.latitude, e.longitude]);
               marker.bindPopup("You are here");
